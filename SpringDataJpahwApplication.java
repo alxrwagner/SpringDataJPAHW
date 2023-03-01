bae0333bb06c01563a49da7b219a796667077012
alxrw <alxr.wagner@gmail.com>
2023-03-01 15:23:35 +0700

    initial commit

---
+++ SpringDataJpahwApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpahw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpahwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpahwApplication.class, args);
+    }
+
+}
